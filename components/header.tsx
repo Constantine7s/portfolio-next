07dd9956dc00da67cd4146e5fbb81552d275d343
Konstantin Shambazov <ks@Konstantins-MacBook-Air.local>
2023-10-19 09:16:01 +0900

    update header styling and add page navigation

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,43 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
+import { links } from "@/lib/data";
+import Link from "next/link";
 
 export default function Header() {
 	return (
 		<header className="z-[999] relative">
-			<div className="fixed top-0 left-1/2 h-[4rem] w-full rounded-none border-white border-opacity-40  bg-white bg-opacity-80 shadow-lg shadow-black/[0.05] backdrop-blur-[0.5rem] sm:h-[3rem] sm:top-6 sm:w-[36rem] sm:rounded-full"></div>
+			<motion.div
+				className="fixed top-0 left-1/2 h-[4.75rem] w-full rounded-none border-white border-opacity-40  bg-white bg-opacity-80 shadow-lg shadow-black/[0.05] backdrop-blur-[0.5rem] sm:h-[3rem] sm:top-6 sm:w-[36rem] sm:rounded-full"
+				initial={{ y: -100, opacity: 0, x: "-50%" }}
+				animate={{ y: 0, opacity: 1, x: "-50%" }}
+				transition={{
+					duration: 0.6,
+					delay: 0.3,
+				}}></motion.div>
+
+			<nav className="fixed flex top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+				<ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+					{links.map((link) => (
+						<motion.li
+							className="h-3/4 flex items-center justify-center"
+							key={link.hash}
+							initial={{ y: -100, opacity: 0 }}
+							animate={{ y: 0, opacity: 1 }}
+							transition={{
+								duration: 0.6,
+								delay: 0.3,
+							}}>
+							<Link
+								className="flex w-full px-3 py-2.5 hover:text-gray-950 transition"
+								href={link.hash}>
+								{link.name}
+							</Link>
+						</motion.li>
+					))}
+				</ul>
+			</nav>
 		</header>
 	);
 }

757b654c0ef146897c357de28834075c983f0441
Konstantin Shambazov <k.shambazov@gmail.com>
2023-10-21 11:40:06 +0900

    update css

--- components/about.tsx
+++ components/about.tsx
@@ -1,21 +1,37 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function About() {
 	return (
-		<section className="flex">
-			<h2>About me</h2>
-			<p>
-				Formerly an entrepreneur, I decided to pursue my passion for programming
-				and enrolled in a coding bootcamp where I studied full-stack software
-				engineering. My favorite part of software development is its
-				problem-solving aspect. As a big fan of puzzle games, I love the feeling
-				of finally figuring out a solution to a challenge. My core stack
-				includes React, Node.js, and PostgreSQL. I'm also proficient with
-				Vue.js, Next.js, MongoDB, AWS, and other technologies, and I'm always
-				eager to learn something new. When I'm not coding, I enjoy exploring
+		<motion.section
+			initial={{ y: 100, opacity: 0 }}
+			animate={{ y: 0, opacity: 1 }}
+			transition={{
+				duration: 0.5,
+				delay: 0.4,
+			}}>
+			<h2 className="text-3xl font-medium capitalize text-center  mb-8">
+				About Me
+			</h2>
+			<p className="mb-3 max-w-[45rem] text-center leading-8">
+				Formerly an <span className="font-medium">entrepreneur</span>, I decided
+				to pursue my passion for programming and enrolled in a coding bootcamp
+				where I studied{" "}
+				<span className="font-medium">full-stack software engineering</span>. My
+				favorite part of software development is its
+				<span className="italic"> problem-solving aspect</span>. As a big fan of
+				puzzle games, I love the feeling of finally figuring out a solution to a
+				challenge. My core stack includes{" "}
+				<span className="font-medium">React, Node.js, and PostgreSQL</span>. I'm
+				also proficient with{" "}
+				<span className="font-medium">Vue.js, Next.js, MongoDB, AWS</span>, and
+				other technologies, and I'm always eager to learn something new.{" "}
+				<span className="italic">When I'm not coding</span>, I enjoy exploring
 				Tokyo, discovering new places, capturing moments with my camera, and
 				tuning in to educational podcasts.
 			</p>
-		</section>
+		</motion.section>
 	);
 }
--- components/divider.tsx
+++ components/divider.tsx
@@ -6,12 +6,12 @@ import { motion } from "framer-motion";
 export default function Divider() {
 	return (
 		<motion.div
-			className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block"
+			className="bg-gray-200 my-24 h-1 w-16 rounded-full hidden sm:block"
 			initial={{ y: 100, opacity: 0 }}
 			animate={{ y: 0, opacity: 1 }}
 			transition={{
 				duration: 0.5,
-				delay: 0.25,
+				delay: 0.3,
 			}}></motion.div>
 	);
 }

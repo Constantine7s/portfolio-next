c8081df84217e6c980444c673e9c337d05b02542
Konstantin Shambazov <k.shambazov@gmail.com>
2023-10-25 15:34:28 +0900

    update divider animation

--- components/divider.tsx
+++ components/divider.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Divider() {
 	return (
-		<div className="bg-gray-200 sm:my-20 my-12 h-1 w-16 rounded-full  sm:block"></div>
+		<motion.div
+			className="bg-gray-200 sm:my-20 my-12 h-1 w-16 rounded-full  sm:block"
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1 }}
+			viewport={{ once: true }}
+			transition={{ duration: 1.5 }}></motion.div>
 	);
 }
